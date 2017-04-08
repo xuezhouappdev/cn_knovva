d1381716fda0a478270685347483a267e3e10fad
Xue Zhou <xuezhouapplication@gmail.com>
2017-04-08 12:44:07 -0400

    updated copies

--- footer.php
+++ footer.php
@@ -35,9 +35,9 @@
 
 
                 <ul>
-                    <li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp <P><a href="https://www.google.com/maps/place/222+Third+St,+Cambridge,+MA+02142/@42.367156,-71.0823859,17z/data=!3m1!4b1!4m5!3m4!1s0x89e370bb6afb650b:0x7bf7a9af0f3613cd!8m2!3d42.3671521!4d-71.0801972" target="_blank">222 3rd Street, Suite 3150, Cambridge, MA &nbsp&nbsp&nbsp&nbsp02142</a></P></li>
+                    <li><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp <P><a href="https://www.google.com/maps/place/222+Third+St,+Cambridge,+MA+02142/@42.367156,-71.0823859,17z/data=!3m1!4b1!4m5!3m4!1s0x89e370bb6afb650b:0x7bf7a9af0f3613cd!8m2!3d42.3671521!4d-71.0801972" target="_blank">&nbsp222 3rd Street, Suite 3150, Cambridge, MA &nbsp&nbsp&nbsp&nbsp&nbsp02142</a></P></li>
                     <li><i class="fa fa-phone" aria-hidden="true"></i>&nbsp<p><a href="callto:6175777770">&nbsp617-577-7770</a></p> </li>
-                    <li> <i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp<p><a href="mailto:info@knovva.com">&nbsp;info@knovva.com</a></p> </li>
+                    <li> <i class="fa fa-envelope-o" aria-hidden="true"></i>&nbsp<p><a href="mailto:info@knovva.com">info@knovva.com</a></p> </li>
 
                 </ul>
 
@@ -62,7 +62,7 @@
 
                 <ul>
                     <li>周一 ~ 周五:   9:00am - 5:00pm</li>
-                    <li>周六 ~ 周日:   Closed</li>
+                    <li>周六 ~ 周日:   休息</li>
                 </ul>
 
                 <div class="col-lg-12 social" style="text-align: center">
