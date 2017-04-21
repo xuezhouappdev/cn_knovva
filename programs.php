ba82ae029bf517d9fb9829bd3b48cfac2aa6547c
Xue Zhou <xuezhouapplication@gmail.com>
2017-04-21 01:20:25 -0400

    updated webinar page

--- programs.php
+++ programs.php
@@ -9,17 +9,17 @@
 
     <link rel="stylesheet" href="bootstrap-3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="vendor/font-awesome-4.7.0/css/font-awesome.css">
 
     <!--font-->
     <link href="https://fonts.googleapis.com/css?family=Parisienne" rel="stylesheet">
 
     <!-- Program -->
     <link rel="stylesheet" href="css/programs.css">
-    <link rel="stylesheet" href="dist/sweetalert.css">
+    <link rel="stylesheet" href="vendor/dist/sweetalert.css">
 
     <!-- sweet alert-->
-    <script src="dist/sweetalert.min.js"></script>
+    <script src="vendor/dist/sweetalert.min.js"></script>
 
 
 
